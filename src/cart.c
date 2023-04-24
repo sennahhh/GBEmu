240eb65ba9ce86175155c922000823083bdb5db5
Hannes Bolmehag <hannes@bolmehag.se>
2023-04-24 21:17:45 +0200

    read cartridge header

--- src/cart.c
+++ src/cart.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-struct cart_header {
+struct parsed_cart_header {
     uint8_t entry_point[4]; 
     uint8_t nintendo_logo[48];  
     char title[16];          
@@ -13,11 +13,12 @@ struct cart_header {
     uint8_t dest_code; 
     uint8_t version_of_game;
     uint8_t header_checksum;
-    uint16_t global_checksum;
+    uint8_t global_checksum[2];
 };
 
-struct cart_header parsed_cart_header;
+struct parsed_cart_header cart_header;
 unsigned char full_cart_header[80];
+unsigned char *full_rom;
 
 int read_cart_header(char *rom_path) {
 
@@ -38,20 +39,23 @@ int parse_cart_header() {
     int i = 0;
     int j = 0;
     uint8_t new_lic_code[2];
+    uint8_t old_lic_code;
     uint8_t cart_type_code;
+    uint8_t rom_size_code;
+    uint8_t ram_size_code;
 
     for(j = 0; j < 4; j++) {
-        parsed_cart_header.entry_point[j] = full_cart_header[i];
+        cart_header.entry_point[j] = full_cart_header[i];
         i++;
     }
 
     for(j = 0; j < 48; j++) {
-        parsed_cart_header.nintendo_logo[j] = full_cart_header[i];
+        cart_header.nintendo_logo[j] = full_cart_header[i];
         i++;
     }
 
     for(j = 0; j < 16; j++) {
-        parsed_cart_header.title[j] = full_cart_header[i];
+        cart_header.title[j] = full_cart_header[i];
         i++;
     }
 
@@ -60,15 +64,34 @@ int parse_cart_header() {
         i++;
     }
     
-    parsed_cart_header.SGB_flag = full_cart_header[i];
+    cart_header.SGB_flag = full_cart_header[i];
     i++;
 
     cart_type_code = full_cart_header[i];
     i++;
 
+    rom_size_code = full_cart_header[i];
+    i++;
 
-
+    ram_size_code = full_cart_header[i];
+    i++;
     
+    cart_header.dest_code = full_cart_header[i];
+    i++;
+
+    old_lic_code = full_cart_header[i];
+    i++;
+
+    cart_header.version_of_game = full_cart_header[i];
+    i++;
+
+    cart_header.header_checksum = full_cart_header[i];
+    i++;
+
+    for(j = 0; j < 2; j++) {
+        cart_header.global_checksum[j] = full_cart_header[i];
+        i++;
+    }
 
     return 0;
 }
