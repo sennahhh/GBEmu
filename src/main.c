d95bccf7cddb1a9b8698f4425962170007e97b99
Hannes Bolmehag <hannes@bolmehag.se>
2023-04-25 19:04:04 +0200

    read cartridge header

--- src/main.c
+++ src/main.c
@@ -7,8 +7,8 @@
 int main(int argc, char **argv) {
     
     if(argc >= 2) {
-        printf("%d\n", read_cart_header(argv[1]));
-        printf("%d\n", parse_cart_header());
+        printf("%d first\n", read_cart_header(argv[1]));
+        printf("%d second\n", parse_cart_header());
     }
     
     return 0;
